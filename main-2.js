789dc849de5cd7e38039554e9f8b225649f5ae5a
Al-Rafi-Biswas <almd88560@gmail.com>
2024-10-03 09:20:28 +0600

    first commit

---
+++ main-2.js
@@ -0,0 +1,107 @@
+
+// first math........
+
+let AppleAse = 25;
+let AroKinlo = 15;
+
+let MotApple = AppleAse + AroKinlo;
+
+
+console.log( "Mot apple ase : " + MotApple + " ti")
+
+
+// second math.......
+
+let JonerCilo = 500;
+let KhorojKorce = 200;
+
+let TerKaseAse = JonerCilo - KhorojKorce;
+
+console.log( "Joner ase : " + TerKaseAse + " taka");
+
+
+// third math...........
+
+let OneHourMake = 120;
+let TotalHour = 8;
+
+let TotalMake = 120 * 8;
+
+console.log("Mot machine toiri korbe : " + TotalMake + " ti")
+
+// fourth math..........
+
+let OneRowTree = 12;
+let TotalRow = 8;
+let AroKinbe = 20;
+
+let AgerTotalGas =  OneRowTree * TotalRow ;
+let AkhonMotGas = AgerTotalGas + AroKinbe ;
+
+console.log( "Bagane Mot gas ase : " + AkhonMotGas + " ti")
+
+
+// fith math........
+
+let RahimerBox = 5;
+let PerBoxKomla = 10;
+let FriendKeDilo = 12;
+
+let MOtKomlaAse = RahimerBox * PerBoxKomla;
+let TerKaseKomlaAse = MOtKomlaAse - FriendKeDilo;
+
+console.log("Rahim er kase mot Komla asse : " + TerKaseKomlaAse + " ti")
+
+// sixth math.............
+
+let ThreeHour = 150;
+let MotHour = 3;
+
+let PerHourJay = ThreeHour / MotHour;
+
+console.log("Train per hour jay : " + PerHourJay + " km")
+
+
+// seventh math...........
+
+// let SixMachineMakeProduct = 200;
+// let UsesMachine = 6;
+// let MotMachine = 8;
+// let NotSale = 3;
+
+
+// let PerMachineMakeProduct = SixMachineMakeProduct / UsesMachine;
+
+// let EightMachineMakeProduct = PerMachineMakeProduct * MotMachine;
+
+// let ObosistoThake = EightMachineMakeProduct % NotSale;
+
+// console.log("Obosisto Product thake mot : " + ObosistoThake + " ti")
+
+
+// seventh math type-2............
+
+let SixMachineMakeProduct = 200;
+let UsesMachine = 6;
+let MotMachine = 8;
+let NotSale = 3;
+
+
+let PerMachineMakeProduct = SixMachineMakeProduct / UsesMachine;
+
+let EightMachineMakeProduct = PerMachineMakeProduct * MotMachine;
+
+let ObosistoThake = EightMachineMakeProduct - NotSale;
+
+console.log("Obosisto Product thake mot : " + ObosistoThake + " ti")
+
+
+// Eighth math ..........
+
+let ChokolateKine = 120;
+let PaketeThake = 9;
+
+let ObosistoAse = ChokolateKine % PaketeThake;
+
+console.log("Dokandarer kasse obosisto thake : " + ObosistoAse + " ti")
+
